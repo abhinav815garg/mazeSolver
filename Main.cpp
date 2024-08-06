8c9bfb11ecb20b3d75699f289fa9152aee77a3a7
abhinav815garg <157962159+abhinav815garg@users.noreply.github.com>
2024-08-06 19:26:57 +0530

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <string>
 #include <stdbool.h>
+#include <stack>
 // using namespace std;
 
 #include "API.h"
@@ -21,9 +22,27 @@ int cy2 = cy1 - 1;
 int cx1 = gridSize / 2;
 int cx2 = cx1 - 1;
 
+int facing = 0;
+
+std::pair<int, int> start = {0, 0};
+
 std::vector<std::pair<int, int>> centers = {{cx1, cy1}, {cx2, cy1}, {cx1, cy2}, {cx2, cy2}};
 
 std::vector<std::vector<int>> directions = {{0, -1}, {1, 0}, {0, 1}, {-1, 0}};
+
+std::set<std::pair<int, int>> verticalWalls = {};
+std::set<std::pair<int, int>> horizontalWalls = {};
+
+std::stack<std::pair<int, int>> toVisit;
+std::deque<std::pair<int, int>> currentPath = {start};
+
+std::set<std::deque<std::pair<int, int>>> allPaths;
+
+std::set<std::pair<int, int>> visited;
+
+std::set<std::deque<std::pair<int, int>>> pathsToCenter;
+
+std::deque<std::pair<int, int>> currPathToGoal;
 
 std::vector<std::vector<int>> floodFill(int gridSize, const std::vector<std::pair<int, int>> &centers, const std::set<std::pair<int, int>> &verticalWalls, const std::set<std::pair<int, int>> &horizontalWalls)
 {
@@ -93,6 +112,8 @@ std::vector<std::vector<int>> floodFill(int gridSize, const std::vector<std::pai
     }
     return distance;
 }
+
+std::vector<std::vector<int>> distances = floodFill(gridSize, centers, verticalWalls, horizontalWalls);
 
 void mouseControl(int currX, int currY, int nextX, int nextY, int &facing)
 {
@@ -219,7 +240,8 @@ int wallSet(int x, int y, int facing, std::set<std::pair<int, int>> &verticalWal
             {
                 verticalWalls.insert({x - 1, y});
             }
-            else {
+            else
+            {
                 API::setWall(x, y, 'w');
             }
         }
@@ -236,7 +258,8 @@ int wallSet(int x, int y, int facing, std::set<std::pair<int, int>> &verticalWal
             {
                 horizontalWalls.insert({x, y - 1});
             }
-            else {
+            else
+            {
                 API::setWall(x, y, 's');
             }
         }
@@ -253,7 +276,8 @@ int wallSet(int x, int y, int facing, std::set<std::pair<int, int>> &verticalWal
             {
                 horizontalWalls.insert({x, y - 1});
             }
-            else {
+            else
+            {
                 API::setWall(x, y, 's');
             }
         }
@@ -263,7 +287,8 @@ int wallSet(int x, int y, int facing, std::set<std::pair<int, int>> &verticalWal
             {
                 verticalWalls.insert({x - 1, y});
             }
-            else {
+            else
+            {
                 API::setWall(x, y, 'w');
             }
         }
@@ -280,7 +305,8 @@ int wallSet(int x, int y, int facing, std::set<std::pair<int, int>> &verticalWal
             {
                 verticalWalls.insert({x - 1, y});
             }
-            else {
+            else
+            {
                 API::setWall(x, y, 'w');
             }
         }
@@ -294,7 +320,8 @@ int wallSet(int x, int y, int facing, std::set<std::pair<int, int>> &verticalWal
             {
                 horizontalWalls.insert({x, y - 1});
             }
-            else {
+            else
+            {
                 API::setWall(x, y, 's');
             }
         }
@@ -341,6 +368,7 @@ bool isCellAccessible(int x, int y, int nx, int ny, const std::set<std::pair<int
             return !verticalWalls.count({x, y});
         }
     }
+    return false;
 }
 
 void exploreCenters(int x, int y, int &facing, std::vector<std::vector<int>> &distances, std::set<std::pair<int, int>> &verticalWalls, std::set<std::pair<int, int>> &horizontalWalls)
@@ -357,7 +385,8 @@ void exploreCenters(int x, int y, int &facing, std::vector<std::vector<int>> &di
         wallSet(8, 7, facing, verticalWalls, horizontalWalls);
         mouseControl(8, 7, 7, 7, facing);
     }
-    else if (x == 8 && y == 7) {
+    else if (x == 8 && y == 7)
+    {
         mouseControl(x, y, 8, 8, facing);
         wallSet(8, 8, facing, verticalWalls, horizontalWalls);
         mouseControl(8, 8, 7, 8, facing);
@@ -366,7 +395,8 @@ void exploreCenters(int x, int y, int &facing, std::vector<std::vector<int>> &di
         wallSet(7, 7, facing, verticalWalls, horizontalWalls);
         mouseControl(7, 7, 8, 7, facing);
     }
-    else if (x == 7 && y == 8) {
+    else if (x == 7 && y == 8)
+    {
         mouseControl(x, y, 8, 8, facing);
         wallSet(8, 8, facing, verticalWalls, horizontalWalls);
         mouseControl(8, 8, 8, 7, facing);
@@ -375,7 +405,8 @@ void exploreCenters(int x, int y, int &facing, std::vector<std::vector<int>> &di
         wallSet(7, 7, facing, verticalWalls, horizontalWalls);
         mouseControl(7, 7, 7, 8, facing);
     }
-    else {
+    else
+    {
         mouseControl(x, y, 8, 7, facing);
         wallSet(8, 7, facing, verticalWalls, horizontalWalls);
         mouseControl(8, 7, 7, 7, facing);
@@ -387,7 +418,6 @@ void exploreCenters(int x, int y, int &facing, std::vector<std::vector<int>> &di
 
     distances = floodFill(gridSize, centers, verticalWalls, horizontalWalls);
 
-
     for (int i = 0; i < gridSize; i++)
     {
         for (int j = 0; j < gridSize; j++)
@@ -397,26 +427,27 @@ void exploreCenters(int x, int y, int &facing, std::vector<std::vector<int>> &di
     }
 }
 
-int main(int argc, char *argv[])
+// std::vector<std::vector<int>> getDirections(int facing) {
+//     if (facing == 0) {
+//         return {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+//     }
+//     else if (facing == 1) {
+//         return {{1, 0}, {0, -1}, {0, 1}, {-1, 0}};
+//     }
+//     else if (facing == 2) {
+//         return {{0, -1}, {-1, 0}, {1, 0}, {0, 1}};
+//     }
+//     else {
+//         return {{-1, 0}, {0, 1}, {0, -1}, {1, 0}};
+//     }
+// }
+
+void exploreMaze(std::pair<int, int> curr, int &facing)
 {
-    log("Running...");
-    API::setColor(0, 0, 'G');
-    API::setText(0, 0, "START");
+    int x = curr.first;
+    int y = curr.second;
 
-    int x = 0;
-    int y = 0;
-
-    int facing = 0;
-    // 0: up, 1: right, 2: down, 3: left
-
-
-    std::deque<std::pair<int, int>> toVisit;
-
-    std::deque<std::pair<int, int>> shortestPath;
-    shortestPath.push_back({x, y});
-
-    std::set<std::pair<int, int>> verticalWalls = {};
-    std::set<std::pair<int, int>> horizontalWalls = {};
+    int wallCount = wallSet(x, y, facing, verticalWalls, horizontalWalls);
 
     std::vector<std::vector<int>> distances = floodFill(gridSize, centers, verticalWalls, horizontalWalls);
 
@@ -428,86 +459,277 @@ int main(int argc, char *argv[])
         }
     }
 
-    while (true)
-    {
-        std::pair<int, int> current = shortestPath.back();
-        x = current.first;
-        y = current.second;
+    // START-1.1
 
-        if ((x == cx1 || x == cx2) && (y == cy1 || y == cy2))
-        {   
-            exploreCenters(x, y, facing, distances, verticalWalls, horizontalWalls);
-            API::setColor(x, y, 'R');
-            int i = shortestPath.size() - 1;
-            for (const auto &cell : shortestPath)
+    bool hasPathToCenter = false;
+    std::deque<std::pair<int, int>> pathFromCell;
+
+    for (const auto &path : pathsToCenter)
+    {
+        if (path.front().first == x && path.front().second == y)
+        {
+            pathFromCell = path;
+            hasPathToCenter = true;
+            break;
+        }
+    }
+    
+    // log(std::to_string(x) + " " + std::to_string(y));    
+    // log(std::to_string(hasPathToCenter));
+    // log("-----------");
+
+    if (hasPathToCenter)
+    {
+        std::deque<std::pair<int, int>> temp = currentPath;
+    
+        if (temp.size() == 0)
+        {
+            return;
+        }
+        temp.pop_back();
+        for (const auto &cell : pathFromCell)
+        {
+            temp.push_back(cell);
+        }
+
+        // if (currPathToGoal == temp) {
+        //     log("True");
+        //     return;
+        // }
+
+        API::clearAllColor();
+        for (const auto &newCell : temp)
+        {
+            API::setColor(newCell.first, newCell.second, 'G');
+        }
+        allPaths.insert(temp);
+
+        // currPathToGoal = temp;
+
+        for (const auto &cell : temp)
+        {
+            bool hasPath = false;
+            std::deque<std::pair<int, int>> tempDeque;
+            std::deque<std::pair<int, int>> ptc;
+            for (const auto &currPath : pathsToCenter)
             {
-                int cx = cell.first;
-                int cy = cell.second;
-                API::setText(cx, cy, std::to_string(i));
-                i--;
-                API::setColor(cx, cy, 'Y');
+                if (currPath.front().first == cell.first && currPath.front().second == cell.second)
+                {
+                    hasPath = true;
+                    ptc = currPath;
+                    break;
+                }
             }
+
+            bool cellFound = false;
+            for (const auto &cellInTemp : temp)
+            {
+                if (cellInTemp.first == cell.first && cellInTemp.second == cell.second)
+                {
+                    cellFound = true;
+                }
+                if (cellFound)
+                {
+                    tempDeque.push_back(cellInTemp);
+                }
+            }
+
+            if (hasPath)
+            {
+                if (tempDeque.size() <= ptc.size())
+                {
+                    pathsToCenter.erase(ptc);
+                }
+            }
+            pathsToCenter.insert(tempDeque);
+        }
+        return;
+    } 
+
+    // END-1.1
+
+    if ((x == cx1 || x == cx2) && (y == cy1 || y == cy2))
+    {
+        visited.insert({x, y});
+        exploreCenters(x, y, facing, distances, verticalWalls, horizontalWalls);
+        API::setColor(x, y, 'R');
+        allPaths.insert(currentPath);
+        // currPathToGoal = currentPath; 
+        API::clearAllColor();
+        // int i = 0;
+        for (const auto &cell : currentPath)
+        {
+            API::setColor(cell.first, cell.second, 'Y');
+            // API::setText(cell.first, cell.second, std::to_string(i));
+            // i++;
+
+            // START-1.2
+
+            bool cellFound = false;
+            std::deque<std::pair<int, int>> tempQ;
+            for (const auto &currCell : currentPath)
+            {
+                if (currCell == cell)
+                {
+                    cellFound = true;
+                }
+                if (cellFound == true)
+                {
+                    tempQ.push_back(currCell);
+                }
+            }
+            pathsToCenter.insert(tempQ);
+
+            // END-1.2
+        }
+        return;
+    }
+
+    // directions = getDirections(facing);          //OPTIMIZE-1
+
+    for (const auto &direction : directions)
+    {
+        int dx = direction[0];
+        int dy = direction[1];
+        int nx = x + dx;
+        int ny = y + dy;
+
+        if (nx < 0 || nx >= gridSize || ny < 0 || ny >= gridSize)
+        {
+            continue;
+        }
+
+        if (((distances[y][x] > distances[ny][nx]) && (distances[y][x] - distances[ny][nx] > 0)) && (isCellAccessible(x, y, nx, ny, verticalWalls, horizontalWalls)))
+        {
+            std::pair<int, int> next = {nx, ny};
+            for (const auto &cell : currentPath)
+            {
+                if (cell == next)
+                {
+                    while (currentPath.size() != 0 && currentPath.back() != next)
+                    {
+                        currentPath.pop_back();
+                    }
+                    if (currentPath.size() != 0)
+                    {
+                        currentPath.pop_back();
+                    }
+                    break;
+                }
+            }
+
+            currentPath.push_back(next);
+
+            API::setColor(nx, ny, 'b');
+
+            mouseControl(x, y, nx, ny, facing);
+
+            visited.insert({x, y});
+
+            exploreMaze(next, facing);
             break;
         }
 
-        int wallCount = wallSet(x, y, facing, verticalWalls, horizontalWalls);
 
-        std::vector<std::vector<int>> distances = floodFill(gridSize, centers, verticalWalls, horizontalWalls);
+    }
 
-        for (int i = 0; i < gridSize; i++)
+    // bool inCurrentPath = false;
+    // for (const auto &cell : currentPath)
+    // {
+    //     if (cell.first == x && cell.second == y)
+    //     {
+    //         log(std::to_string(x) + " " + std::to_string(y));
+    //         inCurrentPath = true;
+    //         break;
+    //     }
+    // }
+
+    if (currentPath.size() == 0 || (currentPath.back().first == x && currentPath.back().second == y))
+    {
+        return;
+    }
+
+    int fromX = currentPath.back().first;
+    int fromY = currentPath.back().second;
+
+    currentPath.pop_back();
+
+    if (currentPath.size() == 0)
+    {
+        return;
+    }
+    int toX = currentPath.back().first;
+    int toY = currentPath.back().second;
+
+    mouseControl(fromX, fromY, toX, toY, facing);
+
+    distances = floodFill(gridSize, centers, verticalWalls, horizontalWalls);
+
+    for (int i = 0; i < gridSize; i++)
+    {
+        for (int j = 0; j < gridSize; j++)
         {
-            for (int j = 0; j < gridSize; j++)
-            {
-                API::setText(j, i, std::to_string(distances[i][j]));
-            }
+            API::setText(j, i, std::to_string(distances[i][j]));
+        }
+    }
+
+    // directions = getDirections(facing);                  //OPTIMIZE-1
+    for (const auto &direction : directions)
+    {
+        int dx = direction[0];
+        int dy = direction[1];
+        int nx = toX + dx;
+        int ny = toY + dy;
+
+        if (nx < 0 || nx >= gridSize || ny < 0 || ny >= gridSize)
+        {
+            continue;
         }
 
-        int minDiff = 999;
-
-        for (const auto &direction : directions)
+        if (distances[toY][toX] > distances[ny][nx] && !visited.count({nx, ny}) && isCellAccessible(toX, toY, nx, ny, verticalWalls, horizontalWalls))
         {
-            int dx = direction[0];
-            int dy = direction[1];
-            int nx = x + dx;
-            int ny = y + dy;
-
-            if (nx < 0 || nx >= gridSize || ny < 0 || ny >= gridSize)
-            {
-                continue;
-            }
-
-            if (((distances[y][x] - distances[ny][nx] < minDiff) && (distances[y][x] - distances[ny][nx] > 0)) && (isCellAccessible(x, y, nx, ny, verticalWalls, horizontalWalls)))
-            {
-                if (minDiff != 999){
-                    toVisit.pop_back();
-                }
-                toVisit.push_back({nx, ny});
-                minDiff = distances[y][x] - distances[ny][nx];
-            }
+            std::pair<int, int> nextCell = {nx, ny};
+            currentPath.push_back(nextCell);
+            API::setColor(toX, toY, 'y');
+            mouseControl(toX, toY, nx, ny, facing);
+            API::setColor(nx, ny, 'b');
+            exploreMaze(nextCell, facing);
+            // return;
+            break;
         }
+    }
+    return;
+}
 
-        std::pair<int, int> next = toVisit.back();
-        int nx = next.first;
-        int ny = next.second;
+int main(int argc, char *argv[])
+{
+    log("Running...");
+    API::setColor(0, 0, 'G');
+    API::setText(0, 0, "START");
 
-        for (const auto &cell: shortestPath)
+    exploreMaze(start, facing);
+
+    log("EXPLORED");
+
+    int shortestPathSize = 999;
+    std::deque<std::pair<int, int>> shortestPath;
+
+    for (const auto &path : allPaths)
+    {
+        if (path.size() < shortestPathSize)
         {
-            if (cell == next)
-            {
-                while (shortestPath.back() != next)
-                {
-                    shortestPath.pop_back();
-                }
-                shortestPath.pop_back();
-                break;
-            }
+            shortestPath = path;
+            shortestPathSize = path.size();
         }
+    }
 
-        shortestPath.push_back(next);
-        toVisit.pop_back();
-
-        API::setColor(nx, ny, 'b');
-
-        mouseControl(x, y, nx, ny, facing);
+    // int i = shortestPathSize - 1;
+    for (const auto &cell : shortestPath)
+    {
+        int cx = cell.first;
+        int cy = cell.second;
+        // API::setText(cx, cy, std::to_string(i));
+        // i--;
+        API::setColor(cx, cy, 'C');
     }
 }
